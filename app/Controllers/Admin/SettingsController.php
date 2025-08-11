1501eecb9c49bd2c45af5c246844d6631123df43
Çınar Özgürgil <cozgurgil@gmail.com>
2025-08-11 16:39:16 +0300

    Admin-Slider-Modül

---
+++ app/Controllers/Admin/SettingsController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use App\Models\SettingsModel;
+use App\Core\BaseController;
+
+
+class SettingsController extends BaseController
+{
+    public function index()
+    {
+
+        $settingsModel = new SettingsModel();
+        $this->settings = $settingsModel->getAllSettings();
+
+        // Verileri view'a gönder
+        $this->renderAdmin(
+            'admin/settings/index',
+            ['settings' => $this->settings] // => Burada model'den aldığımız settings değerini settiğins adında bir değere atarız ve bu değeri bu sayede kullanmış oluruzç.
+        );
+    }
+
+    public function updateSetting()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $key = $_POST['key'];
+            $value = $_POST['value'];
+
+            $settingsModel = new SettingsModel();
+            $result = $settingsModel->updateSetting($key, $value);
+
+            if ($result) {
+                echo json_encode(['status' => 'success', 'message' => 'Ayar başarıyla güncellendi.']);
+            } else {
+                echo json_encode(['status' => 'error', 'message' => 'Ayar güncellenirken bir hata oluştu.']);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
